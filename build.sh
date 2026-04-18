aef81702d3d39734cf7812e4841566f7a6d04406
Ubuntu <ubuntu@ip-172-31-1-61.ec2.internal>
2026-04-18 10:06:07 +0000

    trigger dev pipeline

--- build.sh
+++ build.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
-IMAGE_NAME="ojasvijay/dev:ecommerce"
+ENV=$1
 
-echo "Building Docker Image..."
+if [ "$ENV" == "dev" ]; then
+    IMAGE_NAME="ojasvijay/dev:ecommerce"
+elif [ "$ENV" == "prod" ]; then
+    IMAGE_NAME="ojasvijay/prod:ecommerce"
+else
+    echo "Usage: ./build.sh [dev|prod]"
+    exit 1
+fi
+
+echo "Building Docker Image for $ENV..."
 
 docker build -t $IMAGE_NAME .
 
-echo "Docker Image Built Successfully"
+echo "Docker Image Built: $IMAGE_NAME"
+
